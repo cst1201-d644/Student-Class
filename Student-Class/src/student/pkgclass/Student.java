e2849fed58d50f193e7bd00dc2f13641b8b67220
class <class@CST_N417_09.cst.citytech.cuny.edu>
2015-05-04 17:03:09 -0400

    Student Class HomeWork!!

---
+++ Student-Class/src/student/pkgclass/Student.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student.pkgclass;
+
+/**
+ *
+ * @author class
+ */
+public class Student {
+
+    private String name, address, phoneNum;
+    private int age;
+
+    /**
+     *
+     * @ return Name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name of student
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     *
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     *
+     * @param address of the name
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the phoneNum
+     */
+    public String setPhoneNum() {
+        return phoneNum;
+    }
+
+    /**
+     *
+     * @param phoneNum of the student
+     */
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age of the student
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
